4943d0a9dad6cb5c36b400328578d6bc63b231d4
Hai Le <haile.vnm@gmail.com>
2023-06-25 14:13:54 +0700

    feat: add logger

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express, { Express, Request, Response } from 'express';
 import appRouter from './src/routes';
 import dotenv from 'dotenv';
+import logger from './src/helpers/logger';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/ver', (req: Request, res: Response) => {
   res.json({ build: process.env.BUILD_NUMBER, at: process.env.BUILD_DATE, version: process.env.BUILD_VERSION });
@@ -14,5 +15,5 @@ app.get('/ver', (req: Request, res: Response) => {
 app.use('/', appRouter)
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  logger.info(`✅ [server]: Server is running at http://localhost:${port}`);
 });
